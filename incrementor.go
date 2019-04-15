09f5431a00608aa935818dd659a3e281f66ac850
Abdujabbor <abdujabbor1987@gmail.com>
2019-04-15 11:49:28 +0500

    added counter struct

--- incrementor.go
+++ incrementor.go
@@ -6,19 +6,26 @@ import (
 	"sync"
 )
 
+//Counter struct
+type Counter struct {
+	currentValue int //current stored value in counter
+	maxValue     int //max stored value in counter
+}
+
 //Incrementor  type struct
 type Incrementor struct {
-	currentValue int         //current stored value in incrementor
-	mutex        *sync.Mutex //mutex for detect from data racing
-	maxValue     int         //max value for struct
+	counter Counter     //counter stored value
+	mutex   *sync.Mutex //mutex for detect from data racing
 }
 
 //NewIncrementor method for creating new instance of Incrementor
 func NewIncrementor() *Incrementor {
 	return &Incrementor{
-		maxValue:     1<<31 - 1,
-		mutex:        &sync.Mutex{},
-		currentValue: 0,
+		counter: Counter{
+			currentValue: 0,
+			maxValue:     1<<31 - 1,
+		},
+		mutex: &sync.Mutex{},
 	}
 }
 
@@ -26,18 +33,18 @@ func NewIncrementor() *Incrementor {
 func (i *Incrementor) GetNumber() int {
 	defer i.mutex.Unlock()
 	i.mutex.Lock()
-	return i.currentValue
+	return i.counter.currentValue
 }
 
 //IncrementNumber method for incrementing the current value of Incrementor
 //if incrementor value is equal or greater than max value we will set to zero current value again
 func (i *Incrementor) IncrementNumber() {
-	if i.GetNumber()+1 >= i.maxValue {
+	if i.GetNumber()+1 >= i.counter.maxValue {
 		i.setZeroValue()
 	} else {
 		defer i.mutex.Unlock()
 		i.mutex.Lock()
-		i.currentValue++
+		i.counter.currentValue++
 	}
 }
 
@@ -45,7 +52,7 @@ func (i *Incrementor) IncrementNumber() {
 func (i *Incrementor) setZeroValue() {
 	defer i.mutex.Unlock()
 	i.mutex.Lock()
-	i.currentValue = 0
+	i.counter.currentValue = 0
 }
 
 //SetMaximumValue method for setting max value on Incrementor
@@ -54,8 +61,15 @@ func (i *Incrementor) SetMaximumValue(v int) error {
 		if i.GetNumber() >= v {
 			i.setZeroValue()
 		}
-		i.maxValue = v
+		i.counter.maxValue = v
 		return nil
 	}
 	return fmt.Errorf("You cannot set maximum value less then zero")
+}
+
+//GetMaxValue from counter
+func (i *Incrementor) GetMaxValue() int {
+	defer i.mutex.Unlock()
+	i.mutex.Lock()
+	return i.counter.maxValue
 }
--- incrementor_test.go
+++ incrementor_test.go
@@ -7,6 +7,11 @@ import (
 
 func TestIncrement(t *testing.T) {
 	n := NewIncrementor()
+
+	if n.GetMaxValue() != 1<<31-1 {
+		t.Errorf("Error while initilize max value for counter")
+	}
+
 	if n.GetNumber() != 0 {
 		t.Error("Error while initilize incrementor value")
 	}
@@ -63,5 +68,22 @@ func TestConcurrencyIncrementing(t *testing.T) {
 	wg.Wait()
 	if inc.GetNumber() != 1000 {
 		t.Errorf("Error while on concurrency incrementing, with answer %d", inc.GetNumber())
+	}
+}
+
+func TestConcurrencyIncrementingAndMaxValue(t *testing.T) {
+	inc := NewIncrementor()
+	inc.SetMaximumValue(2)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			inc.IncrementNumber()
+		}()
+	}
+	wg.Wait()
+	if inc.GetNumber() != 0 {
+		t.Errorf("error while on concurrency incrementing and max value, with answer: %d", inc.GetNumber())
 	}
 }
